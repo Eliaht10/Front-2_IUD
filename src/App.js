3f880ae5883a0f3e02242d35e779074d392bc7a7
Eliaht10 <129532884+Eliaht10@users.noreply.github.com>
2023-04-16 18:48:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TipoEquipos from "./components/TipoEquipos";
+import AppRouter from "./routes/AppRouter";
+
+function App() {
+  
+  return (
+    <AppRouter />
+  );
+}
+
+export default App;
